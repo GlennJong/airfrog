31d92791e805742445ce9fd8edb50e3405cad4c3
CHH <chh@chh.tw>
2016-07-24 17:33:30 +0800

    開始試著抓取並且整理 JSON 資料然後顯示到頁面上

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,46 @@
 
     'use strict'
 
-    $.getJSON('AQX.json')
-     .done(onDone)
-     .fail(onFail)
+    new Vue({
+        el: "#app",
 
+        data: {
+            data: []
+        },
 
-    function onDone(data, status, xhr) {
-        debugger
-    }
+        methods: {
+            onDone: function (data, status, xhr) {
+                this.data = data
+            },
 
-    function onFail(xhr, errorType, err) {
-        debugger
-    }
+            onFail: function (xhr, errorType, err) {
+                // on fail...
+            },
+        },
+
+        computed: {
+            grouped_data: function () {
+                let data = {}
+
+                for (var i = this.data.length - 1; i >= 0; i--) {
+                    let obj = this.data[i]
+
+                    if (data[obj.County] === undefined) {
+                        data[obj.County] = [obj]
+                    }
+
+                    data[obj.County].push(obj)
+                }
+
+                return data
+            }
+        },
+
+        ready: function () {
+            $.getJSON('AQX.json')
+             .done(this.onDone)
+             .fail(this.onFail)
+        }
+    })
 
 })()
